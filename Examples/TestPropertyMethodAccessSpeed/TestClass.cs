d08d246057ef77932e8895c1d70e76245e9b1e86
André Spitzner <bcare@becee.de>
2016-09-09 23:15:59 +0200

    First checkin

---
+++ Examples/TestPropertyMethodAccessSpeed/TestClass.cs
@@ -0,0 +1,63 @@
+﻿
+namespace TestPropertyMethodAccessSpeed
+{
+	/// <summary>
+	/// Test class
+	/// </summary>
+	public class TestClass
+	{
+		/// <summary>
+		/// Gets or sets the property string.
+		/// </summary>
+		/// <value>
+		/// The property string.
+		/// </value>
+		public string PropertyString { get; set; }
+
+		/// <summary>
+		/// The property string backing store
+		/// </summary>
+		private string _PropertyStringBackingStore;
+
+		/// <summary>
+		/// Gets or sets the property string backing store.
+		/// </summary>
+		/// <value>
+		/// The property string backing store.
+		/// </value>
+		public string PropertyStringBackingStore
+		{
+			get { return _PropertyStringBackingStore; }
+			set
+			{
+				if (_PropertyStringBackingStore != null
+					&& value != null
+					&& _PropertyStringBackingStore.Equals(value))
+					return;
+
+				_PropertyStringBackingStore = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets the property.
+		/// </summary>
+		/// <returns></returns>
+		public string GetProperty()
+		{
+			return _PropertyStringBackingStore;
+		}
+
+		/// <summary>
+		/// Sets the property.
+		/// </summary>
+		/// <param name="newString">The new string.</param>
+		public void SetProperty(string newString)
+		{
+			if (_PropertyStringBackingStore.Equals(newString))
+				return;
+
+			_PropertyStringBackingStore = newString;
+		}
+	}
+}
